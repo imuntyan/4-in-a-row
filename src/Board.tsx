4342479b3a1b9087e5d7ca1276503e532c2a432e
Igor Muntyan <igor.muntyan@gmail.com>
2021-01-12 21:09:18 -0500

    CSS vars

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import './Board.css';
 
 class Board extends React.Component {
+
+    boardRef: any;
+
     constructor(props: any) {
         super(props);
+        this.boardRef = createRef()
+    }
+
+    componentDidMount() {
+        this.init();
+    }
+
+    init() {
+        const board = this.boardRef.current;
+
+        // board.style.setProperty("", "")
+        console.log(board)
+        board.style.setProperty("--colNum", "3")
+
     }
 
     render() {
+
         return (
-            <div className="game-board">
+            <div ref={this.boardRef} className="game-board">
                 <div className="box"></div>
                 <div className="box"></div>
                 <div className="box"></div>
